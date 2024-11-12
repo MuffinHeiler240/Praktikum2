a1ac91aa3110814a0079e38e37468d4a5d4b4eaa
MuffinHeiler240 <jonathangreive@gmail.com>
2024-11-12 13:21:42 +0100

    fixed comments zahlen.h

--- zahlen.h
+++ zahlen.h
@@ -1,14 +1,14 @@
-/** Verwende zur Darstellung maximal 8 DIGITS lange Zeichenketten */
+/* Verwende zur Darstellung maximal 8 DIGITS lange Zeichenketten */
 #define DIGITS 8
 
 /* Groesste erlaubte Zahl mit DIGITS Stellen */
 #define MAXZAHL (1<<DIGITS)
 
-/** Initialisiere /loesche str mit c. */
+/* Initialisiere /loesche str mit c. */
 void clear(char c, char str []) ;
 
-/** Bilde aus der Zeichenkette str eine positive Ganzzahl und gebe diese zurueck oder −1 im Fehlerfall einer ungueltigen Zeichenkette */
+/* Bilde aus der Zeichenkette str eine positive Ganzzahl und gebe diese zurueck oder −1 im Fehlerfall einer ungueltigen Zeichenkette */
 int str2int (char str []) ;
 
-/** Schreibt die Binaerdarstellung der Ganzzahl i in die Zeichenkette str */
+/* Schreibt die Binaerdarstellung der Ganzzahl i in die Zeichenkette str */
 void int2Binaer(int i, char str []) ;
